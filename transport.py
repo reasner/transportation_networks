2e895e2b75f6003474815f25cd37e605b8a06e43
reasner <msreasner@gmail.com>
2021-01-01 17:17:25 -0600

    water and rail maps

--- transport.py
+++ transport.py
@@ -42,28 +42,50 @@ us_land_map = gpd.overlay(land_map,bounding_box,how='intersection')
 us_land_map['all'] = 1
 us_land_map = us_land_map.dissolve(by='all')
 
-#plot
+#RAIL
+rail_path = os.path.join(cd,r'tl_2010_us_rails',r'tl_2010_us_rails.shp')
+rail_map = gpd.read_file(rail_path)
+warnings.filterwarnings("ignore")
+rail_map = rail_map.to_crs("EPSG:4326")
+warnings.filterwarnings("default")
+rail_map = gpd.clip(rail_map,bounding_box)
+rail_map = rail_map[rail_map['MTFCC'] == 'R1011']
 fig, ax = plt.subplots(1, figsize=(8,6))
 ax.axis('off')
-us_land_map.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='gray',antialiased=False)
-plt.show()
-
-#RAIL
-
+rail_map.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='black',antialiased=False)
+bounding_box.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='white',antialiased=False)
+rail_img_path = os.path.join(cd,r'rail_map.png')
+plt.savefig(rail_img_path,bbox_inches='tight',dpi=100)
 
 #ROAD
 road_path = os.path.join(cd,r'tl_2016_us_primaryroads',r'tl_2016_us_primaryroads.shp')
-road_map = gpd.read_file(road_path,mask=us_land_map)
+road_map = gpd.read_file(road_path)
+warnings.filterwarnings("ignore")
 road_map = road_map.to_crs("EPSG:4326")
+warnings.filterwarnings("default")
+road_map = gpd.clip(road_map,bounding_box)
 fig, ax = plt.subplots(1, figsize=(8,6))
 ax.axis('off')
 road_map.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='black',antialiased=False)
-us_land_map.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='gray',antialiased=False)
-plt.show()
-
+bounding_box.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='white',antialiased=False)
+road_img_path = os.path.join(cd,r'road_map.png')
+plt.savefig(road_img_path,bbox_inches='tight',dpi=100)
 
 #WATER
-
+water_path = os.path.join(cd,r'CNW_v3_NAD83',r'CNW_v3_NAD83.shp')
+water_map = gpd.read_file(water_path)
+warnings.filterwarnings("ignore")
+water_map = water_map.to_crs("EPSG:4326")
+warnings.filterwarnings("default")
+water_map = gpd.clip(water_map,bounding_box)
+fig, ax = plt.subplots(1, figsize=(8,6))
+ax.axis('off')
+bounding_box.plot(ax=ax,facecolor="black",linewidth=0.1,edgecolor='none',antialiased=False)
+us_land_map.plot(ax=ax,facecolor="white",linewidth=0.1,edgecolor='none',antialiased=False)
+water_map.plot(ax=ax,facecolor="none",linewidth=0.2,edgecolor='black',antialiased=False)
+bounding_box.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='white',antialiased=False)
+water_img_path = os.path.join(cd,r'water_map.png')
+plt.savefig(water_img_path,bbox_inches='tight',dpi=100)
 
 
 
