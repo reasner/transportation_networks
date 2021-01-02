b9099d32f57d1c4b497489aabee4f5800a2c2ff2
reasner <msreasner@gmail.com>
2021-01-02 12:36:48 -0600

    centroids of czs and bounds

--- transport.py
+++ transport.py
@@ -23,20 +23,17 @@ warnings.filterwarnings("default")
 county_map['cont_us'] = 1
 cont_us_map = county_map.dissolve(by='cont_us')
 
-#load land old projection
+#BOUNDING BOX
 land_path = os.path.join(cd,r'Longitude_Graticules_and_World_Countries_Boundaries-shp',r'99bfd9e7-bb42-4728-87b5-07f8c8ac631c2020328-1-1vef4ev.lu5nk.shp')
 land_map = gpd.read_file(land_path)
-
 #reproject/buffer
 warnings.filterwarnings("ignore")
 land_map = land_map.to_crs("EPSG:4326")
 land_map['geometry'] = land_map['geometry'].buffer(0.0001)
 warnings.filterwarnings("default")
-
 #load create geodataframe from bounding box
 bbox = cont_us_map.envelope
 bounding_box = gpd.GeoDataFrame(gpd.GeoSeries(bbox), columns=['geometry'])
-
 #combine
 us_land_map = gpd.overlay(land_map,bounding_box,how='intersection')
 us_land_map['all'] = 1
@@ -87,5 +84,45 @@ bounding_box.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='white',antiali
 water_img_path = os.path.join(cd,r'water_map.png')
 plt.savefig(water_img_path,bbox_inches='tight',dpi=100)
 
-
-
+#LOCATIONS
+cz_crosswalk_path = os.path.join(cd,r'cz00_eqv_v1.xls')
+cz_crosswalk_xls = pd.ExcelFile(cz_crosswalk_path,engine='xlrd')
+cz_crosswalk_xls_sheet_names = cz_crosswalk_xls.sheet_names
+cz_crosswalk = cz_crosswalk_xls.parse(cz_crosswalk_xls_sheet_names[0])
+cz_crosswalk = cz_crosswalk[['FIPS','Commuting Zone ID, 2000']]
+cz_crosswalk['FIPS'] = cz_crosswalk['FIPS'].apply(str)
+cz_crosswalk['FIPS'] = cz_crosswalk['FIPS'].str.zfill(5)
+cz_crosswalk['Commuting Zone ID, 2000'] = cz_crosswalk['Commuting Zone ID, 2000'].apply(str)
+cz_crosswalk['Commuting Zone ID, 2000'] = cz_crosswalk['Commuting Zone ID, 2000'].str.zfill(3)
+cz_crosswalk.columns = ['fips', 'cz_area']
+##join county map and cfs crosswalk
+cz_comb_df = pd.merge(county_map,cz_crosswalk,on='fips',how='inner')
+cz_map = cz_comb_df.dissolve(by='cz_area')
+cz_map.reset_index(inplace=True)
+cz_map = cz_map[['cz_area','geometry']]
+##cz
+cz_codes = cz_map['cz_area'].tolist()
+cz_centroids_df = cz_map[['geometry']].copy()
+warnings.filterwarnings("ignore")
+cz_centroids_df.geometry = cz_centroids_df.centroid
+cz_lon = cz_centroids_df.centroid.x.tolist()
+cz_lat = cz_centroids_df.centroid.y.tolist()
+warnings.filterwarnings("default")
+cz_out = [cz_codes,cz_lon,cz_lat]
+cz_out_df = pd.DataFrame(cz_out)
+cz_out_df = cz_out_df.T
+cz_out_df.columns = ['area','lon','lat']
+cz_out_path = os.path.join(cd,r'cz_loc.csv')
+cz_out_df.to_csv(cz_out_path,index=False)
+#map
+fig, ax = plt.subplots(1, figsize=(8.5,6.5))
+ax.axis('off')
+cz_centroids_df.plot(ax=ax,marker=',',color='red',markersize=1,antialiased=False)
+bounding_box.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='white',antialiased=False)
+cent_img_path = os.path.join(cd,r'cent_map.png')
+plt.savefig(cent_img_path,bbox_inches='tight',dpi=100)
+#bounding box to translate locations to pixels
+bbox_bounds = bounding_box.bounds
+bbox_bounds = bbox_bounds.T
+bbox_out_path = os.path.join(cd,r'bbox_bounds.csv')
+bbox_bounds.to_csv(bbox_out_path)
