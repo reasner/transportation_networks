20a69a92940d46ed8dddecf019acafdd4d6ff71d
reasner <msreasner@gmail.com>
2021-01-19 12:31:28 -0500

    reproject, add cfs and counties, and clean up

--- transport.py
+++ transport.py
@@ -10,17 +10,11 @@ import warnings
 from PIL import Image
 
 # SETUP
-cd = os.path.join(os.path.expanduser("~"),r'Documents',r'Projects',r'transport')
-
+cd = os.path.join(os.path.expanduser("~"),r'Documents',r'projects',r'transport')
+cd_dotdot = os.path.join(os.path.expanduser("~"),r'Documents',r'projects')
 # LOAD COUNTY SHAPEFILE
-shapefile_path = os.path.join(cd,r'gz_2010_us_050_00_500k',r'gz_2010_us_050_00_500k.shp')
+shapefile_path = os.path.join(cd_dotdot,r'cfs_cz_shapefile_and_distances',r'fips',r'fips.shp')
 county_map = gpd.read_file(shapefile_path)
-county_map = county_map[(county_map['STATE'] != '02') & (county_map['STATE'] != '15') & (county_map['STATE'] != '72')]
-county_map['fips'] = county_map['STATE'] + county_map['COUNTY']
-warnings.filterwarnings("ignore")
-county_map = county_map.to_crs("EPSG:4326")
-county_map['geometry'] = county_map['geometry'].buffer(0.0001)
-warnings.filterwarnings("default")
 county_map['cont_us'] = 1
 cont_us_map = county_map.dissolve(by='cont_us')
 
@@ -29,8 +23,8 @@ land_path = os.path.join(cd,r'Longitude_Graticules_and_World_Countries_Boundarie
 land_map = gpd.read_file(land_path)
 #reproject/buffer
 warnings.filterwarnings("ignore")
-land_map = land_map.to_crs("EPSG:4326")
-land_map['geometry'] = land_map['geometry'].buffer(0.0001)
+land_map = land_map.to_crs("EPSG:5070")
+land_map['geometry'] = land_map['geometry'].buffer(5)
 warnings.filterwarnings("default")
 #load create geodataframe from bounding box
 bbox = cont_us_map.envelope
@@ -49,7 +43,7 @@ bbox_bounds.to_csv(bbox_out_path)
 water_path = os.path.join(cd,r'CNW_v3_NAD83',r'CNW_v3_NAD83.shp')
 water_map = gpd.read_file(water_path)
 warnings.filterwarnings("ignore")
-water_map = water_map.to_crs("EPSG:4326")
+water_map = water_map.to_crs("EPSG:5070")
 warnings.filterwarnings("default")
 water_map = gpd.clip(water_map,bounding_box)
 fig = plt.figure()
@@ -67,7 +61,7 @@ plt.savefig(water_img_path,bbox_inches='tight',pad_inches=0,dpi=100)
 road_path = os.path.join(cd,r'tl_2016_us_primaryroads',r'tl_2016_us_primaryroads.shp')
 road_map = gpd.read_file(road_path)
 warnings.filterwarnings("ignore")
-road_map = road_map.to_crs("EPSG:4326")
+road_map = road_map.to_crs("EPSG:5070")
 warnings.filterwarnings("default")
 road_map = gpd.clip(road_map,bounding_box)
 fig = plt.figure()
@@ -84,7 +78,7 @@ plt.savefig(road_img_path,bbox_inches='tight',pad_inches=0,dpi=100)
 rail_path = os.path.join(cd,r'tl_2010_us_rails',r'tl_2010_us_rails.shp')
 rail_map = gpd.read_file(rail_path)
 warnings.filterwarnings("ignore")
-rail_map = rail_map.to_crs("EPSG:4326")
+rail_map = rail_map.to_crs("EPSG:5070")
 warnings.filterwarnings("default")
 rail_map = gpd.clip(rail_map,bounding_box)
 rail_map = rail_map[rail_map['MTFCC'] == 'R1011']
@@ -99,22 +93,11 @@ rail_img_path = os.path.join(cd,r'rail_map.png')
 plt.savefig(rail_img_path,bbox_inches='tight',pad_inches=0,dpi=100)
 
 #LOCATIONS
-cz_crosswalk_path = os.path.join(cd,r'cz00_eqv_v1.xls')
-cz_crosswalk_xls = pd.ExcelFile(cz_crosswalk_path,engine='xlrd')
-cz_crosswalk_xls_sheet_names = cz_crosswalk_xls.sheet_names
-cz_crosswalk = cz_crosswalk_xls.parse(cz_crosswalk_xls_sheet_names[0])
-cz_crosswalk = cz_crosswalk[['FIPS','Commuting Zone ID, 2000']]
-cz_crosswalk['FIPS'] = cz_crosswalk['FIPS'].apply(str)
-cz_crosswalk['FIPS'] = cz_crosswalk['FIPS'].str.zfill(5)
-cz_crosswalk['Commuting Zone ID, 2000'] = cz_crosswalk['Commuting Zone ID, 2000'].apply(str)
-cz_crosswalk['Commuting Zone ID, 2000'] = cz_crosswalk['Commuting Zone ID, 2000'].str.zfill(3)
-cz_crosswalk.columns = ['fips', 'cz_area']
-##join county map and cfs crosswalk
-cz_comb_df = pd.merge(county_map,cz_crosswalk,on='fips',how='inner')
-cz_map = cz_comb_df.dissolve(by='cz_area')
-cz_map.reset_index(inplace=True)
-cz_map = cz_map[['cz_area','geometry']]
-##cz
+if not os.path.exists(os.path.join(cd,r'pairs')):
+    os.makedirs(os.path.join(cd,r'pairs'))
+#Commuting Zones (CZ)
+cz_map_path = os.path.join(cd_dotdot,r'cfs_cz_shapefile_and_distances',r'cz00',r'cz00.shp')
+cz_map = gpd.read_file(cz_map_path)
 cz_codes = cz_map['cz_area'].tolist()
 cz_centroids_df = cz_map[['geometry']].copy()
 warnings.filterwarnings("ignore")
@@ -146,25 +129,96 @@ for entry in cz_distances:
     dest_cz_code.append(entry[3])
 cz_out = {'orig_lon':orig_cz_lon,'orig_lat':orig_cz_lat,'dest_lon':dest_cz_lon,'dest_lat':dest_cz_lat,'orig_code':orig_cz_code,'dest_code':dest_cz_code}  
 cz_out_df = pd.DataFrame(cz_out)
-cz_out_path = os.path.join(cd,r'cz_pair.csv')
+cz_out_path = os.path.join(cd,r'pairs',r'cz_pair.csv')
 cz_out_df.to_csv(cz_out_path,index=False)
 #map of locations
 fig, ax = plt.subplots(1, figsize=(8.5,6.5))
 ax.axis('off')
 cz_centroids_df.plot(ax=ax,marker=',',color='red',markersize=1,antialiased=False)
 bounding_box.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='white',antialiased=False)
-cent_img_path = os.path.join(cd,r'cent_map.png')
-plt.savefig(cent_img_path,bbox_inches='tight',dpi=100)
-#adjust images
-rail_image = Image.open(rail_img_path)
-rail_width, rail_height = rail_image.size
-rail_box = (0,1,rail_width,rail_height)
-rail_region = rail_image.crop(rail_box)
-rail_region.save(rail_img_path)
-
-road_image = Image.open(road_img_path)
-road_width, road_height = road_image.size
-road_box = (0,2,road_width,road_height)
-road_region = road_image.crop(road_box)
-road_region.size
-road_region.save(road_img_path)
+cz_cent_img_path = os.path.join(cd,r'cz_cent_map.png')
+plt.savefig(cz_cent_img_path,bbox_inches='tight',pad_inches=0,dpi=100)
+#Commodity Flow Survery (CFS) Areas
+cfs_map_path = os.path.join(cd_dotdot,r'cfs_cz_shapefile_and_distances',r'cfs07',r'cfs07.shp')
+cfs_map = gpd.read_file(cfs_map_path)
+cfs_codes = cfs_map['cfs_area'].tolist()
+cfs_centroids_df = cfs_map[['geometry']].copy()
+warnings.filterwarnings("ignore")
+cfs_centroids_df.geometry = cfs_centroids_df.centroid
+cfs_lon = cfs_centroids_df.centroid.x.tolist()
+cfs_lat = cfs_centroids_df.centroid.y.tolist()
+warnings.filterwarnings("default")
+cfs_areas = list(zip(cfs_lon,cfs_lat))
+cfs_distances = []
+for ind1,cfs1 in enumerate(cfs_areas):
+    for ind2,cfs2 in enumerate(cfs_areas):
+        orig_cfs_code = cfs_codes[ind1]
+        dest_cfs_code = cfs_codes[ind2]
+        local_list = [cfs1,cfs2,orig_cfs_code,dest_cfs_code]
+        cfs_distances.append(local_list)
+orig_cfs_lon = []
+orig_cfs_lat = []
+dest_cfs_lon = []
+dest_cfs_lat = []
+dist_cfs = []
+orig_cfs_code = []
+dest_cfs_code = []
+for entry in cfs_distances:
+    orig_cfs_lon.append(entry[0][0])
+    orig_cfs_lat.append(entry[0][1])
+    dest_cfs_lon.append(entry[1][0])
+    dest_cfs_lat.append(entry[1][1])
+    orig_cfs_code.append(entry[2])
+    dest_cfs_code.append(entry[3])
+cfs_out = {'orig_lon':orig_cfs_lon,'orig_lat':orig_cfs_lat,'dest_lon':dest_cfs_lon,'dest_lat':dest_cfs_lat,'orig_code':orig_cfs_code,'dest_code':dest_cfs_code}  
+cfs_out_df = pd.DataFrame(cfs_out)
+cfs_out_path = os.path.join(cd,r'pairs',r'cfs_pair.csv')
+cfs_out_df.to_csv(cfs_out_path,index=False)
+#map of locations
+fig, ax = plt.subplots(1, figsize=(8.5,6.5))
+ax.axis('off')
+cfs_centroids_df.plot(ax=ax,marker=',',color='red',markersize=1,antialiased=False)
+bounding_box.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='white',antialiased=False)
+cfs_cent_img_path = os.path.join(cd,r'cfs_cent_map.png')
+plt.savefig(cfs_cent_img_path,bbox_inches='tight',pad_inches=0,dpi=100)
+#Counties (cty) 
+cty_codes = county_map['fips'].tolist()
+cty_centroids_df = county_map[['geometry']].copy()
+warnings.filterwarnings("ignore")
+cty_centroids_df.geometry = cty_centroids_df.centroid
+cty_lon = cty_centroids_df.centroid.x.tolist()
+cty_lat = cty_centroids_df.centroid.y.tolist()
+warnings.filterwarnings("default")
+cty_areas = list(zip(cty_lon,cty_lat))
+cty_distances = []
+for ind1,cty1 in enumerate(cty_areas):
+    for ind2,cty2 in enumerate(cty_areas):
+        orig_cty_code = cty_codes[ind1]
+        dest_cty_code = cty_codes[ind2]
+        local_list = [cty1,cty2,orig_cty_code,dest_cty_code]
+        cty_distances.append(local_list)
+orig_cty_lon = []
+orig_cty_lat = []
+dest_cty_lon = []
+dest_cty_lat = []
+dist_cty = []
+orig_cty_code = []
+dest_cty_code = []
+for entry in cty_distances:
+    orig_cty_lon.append(entry[0][0])
+    orig_cty_lat.append(entry[0][1])
+    dest_cty_lon.append(entry[1][0])
+    dest_cty_lat.append(entry[1][1])
+    orig_cty_code.append(entry[2])
+    dest_cty_code.append(entry[3])
+cty_out = {'orig_lon':orig_cty_lon,'orig_lat':orig_cty_lat,'dest_lon':dest_cty_lon,'dest_lat':dest_cty_lat,'orig_code':orig_cty_code,'dest_code':dest_cty_code}  
+cty_out_df = pd.DataFrame(cty_out)
+cty_out_path = os.path.join(cd,r'pairs',r'cty_pair.csv')
+cty_out_df.to_csv(cty_out_path,index=False)
+#map of locations
+fig, ax = plt.subplots(1, figsize=(8.5,6.5))
+ax.axis('off')
+cty_centroids_df.plot(ax=ax,marker=',',color='red',markersize=1,antialiased=False)
+bounding_box.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='white',antialiased=False)
+cty_cent_img_path = os.path.join(cd,r'cty_cent_map.png')
+plt.savefig(cty_cent_img_path,bbox_inches='tight',pad_inches=0,dpi=100)
